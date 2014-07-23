1bc02b150e382427fcc5ac2d37ce8b3c42fb9505
aerhard <a_erhard@gmx.de>
2014-07-23 04:42:15 +0200

    added UI test file

---
+++ test/core/UI.js
@@ -0,0 +1,29 @@
+define(['core/UI'], function (UI) {
+
+  describe("UI", function () {
+
+    it("is defined", function () {
+      expect(UI).toBeDefined();
+    });
+
+//    it("logs when logging is enabled", function () {
+//      spyOn(Vex, 'L');
+//      UI.setEnabled(true);
+//      expect(UI.enabled).toEqual(true);
+//      UI.log('a', 'b');
+//      expect(Vex.L).toHaveBeenCalledWith('MEISnippetViewer', { 0 : 'a', 1 : 'b' });
+//    });
+//
+//    it("doesn't log when logging is disabled", function () {
+//      spyOn(Vex, 'L');
+//      UI.setEnabled(false);
+//      expect(UI.enabled).toEqual(false);
+//      UI.log('a', 'b');
+//      expect(Vex.L).not.toHaveBeenCalled();
+//    });
+
+  });
+
+});
+
+
